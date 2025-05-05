0e20de81b1e2b4473af2d8edb9764603bb750273
shredder11205 <baseballwinser@gmail.com>
2025-05-04 19:08:16 -0700

    Revamped home page

--- js/site.js
+++ js/site.js
@@ -1,21 +1,9 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+document.addEventListener('DOMContentLoaded', () => {
+  const ctoToggle = document.getElementById('ctoToggle');
+  const ctoImages = document.getElementById('ctoImages');
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+  ctoToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    ctoImages.classList.toggle('hidden');
+  });
+});
