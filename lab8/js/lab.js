e5fa7d2a11fdd6e7c79da724b866b1dba760cd9f
shredder11205 <baseballwinser@gmail.com>
2025-05-08 18:13:58 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,26 @@
+/*
+ * Author: Walter Serwin <wserwin@ucsc.edu>
+ * Created: 8 March
+ * License: Public Domain
+*/
+
+function isEven(x) {
+    return (x % 2 == 0);
+}
+
+
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
+
+array = [100, 81, 4, 16, 42, 144, 10000];
+console.log("My array", array);
+
+var result = array.map(isEven);
+
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x){
+    return x ** 0.5;
+});
+
+console.log("Squareroot of array:", result);
